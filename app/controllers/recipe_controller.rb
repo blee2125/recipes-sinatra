25ef13c01a629bbaf3684ce428253277907803bc
blee2125 <blee2125@gmail.com>
2020-04-28 10:20:11 -0400

    add route for recipes

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,3 +1,12 @@
 class RecipeController < ApplicationController
 
+    get '/recipes' do
+        if logged_in?
+            @recipes= Recipe.all
+            erb :'recipes/index'
+        else
+            redirect to "/"
+        end
+    end
+
 end

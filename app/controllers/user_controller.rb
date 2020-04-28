549686fb176b5dc1d1901084440d504e2cc23ef8
blee2125 <blee2125@gmail.com>
2020-04-28 11:24:04 -0400

    add post route for signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,27 @@
 class UserController < ApplicationController
 
+    
+
+    get '/signup' do
+        if !session[:user_id]
+            erb :'/user/new'
+        else
+            redirect to '/recipes'
+        end
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user= User.create(:username => params[:username], :password => params[:password])
+            session[:user_id]= @user.id
+            redirect to '/recipes'
+        end
+    end
+
+    get '/login' do
+        erb :'/user/login'
+    end
+
 end

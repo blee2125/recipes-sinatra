d53daecc4a46bb7d401f5536c4daf7f4f2944202
blee2125 <blee2125@gmail.com>
2020-04-29 16:47:13 -0400

    post route for recipe input

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -18,4 +18,18 @@ class RecipeController < ApplicationController
         end
     end
 
+    post '/recipes' do
+        if logged_in?
+            if params[:recipe_name] == "" || params[:ingredients] == "" || params[:directions] == ""
+                redirect to '/recipes/new'
+            else
+                @recipe= current_user.recipes.create(name: params[:recipe_name],
+                    ingredients: params[:ingredients].split(/\r\n/),
+                    directions: params[:directions].split(/\r\n/))
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
 end

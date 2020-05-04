183a83674392debe2385b5f10189ad0d2bac15f2
blee2125 <blee2125@gmail.com>
2020-05-04 13:29:56 -0400

    modify post /recipes

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -18,14 +18,20 @@ class RecipeController < ApplicationController
         end
     end
 
+    get '/recipes/:slug' do
+        @recipe= Recipe.find_by_slug(params[:slug])
+        erb :'recipes/show'
+    end
+
     post '/recipes' do
         if logged_in?
             if params[:recipe_name] == "" || params[:ingredients] == "" || params[:directions] == ""
                 redirect to '/recipes/new'
             else
+                
                 @recipe= current_user.recipes.create(name: params[:recipe_name],
-                    ingredients: params[:ingredients].split(/\r\n/),
-                    directions: params[:directions].split(/\r\n/))
+                    ingredients: params[:ingredients],
+                    directions: params[:directions])
                 redirect to '/recipes'
             end
         else
